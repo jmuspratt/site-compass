e60dafa8fe8bd06cdccb79a2c64fc22ac1a140bc
James Muspratt <jmuspratt@gmail.com>
2014-04-28 17:11:35 -0600

    jquery 1.11.0

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
 		<![endif]-->
 		
 		<!-- js  -->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script>window.jQuery || document.write("<script src='js/jquery-1.9.1.min.js'>\x3C/script>")</script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script>window.jQuery || document.write("<script src='js/jquery-1.11.0.min.js'>\x3C/script>")</script>
 		<script src="js/modernizr.custom.75579.js"></script>
 		<script src="js/placeholders.min.js"></script>
 		<script src="js/scripts.js"></script>
