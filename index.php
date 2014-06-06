3883b9324e9c607bf31536f76a9e38e3cb9f2916
James Muspratt <jmuspratt@gmail.com>
2014-06-06 09:44:01 -0600

    rename lt-ie-9

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 		<!--[if lt IE 9]>
-			<link rel="stylesheet" type="text/css" href="css/lt-ie9.css" media="screen" />
+			<link rel="stylesheet" type="text/css" href="css/screen-lt-ie9.css" media="screen" />
 		<![endif]-->
 		
 		<!-- js  -->
