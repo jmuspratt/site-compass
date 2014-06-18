48c198209a64496305b41b932cdc25c84f6eba1b
James Muspratt <jmuspratt@gmail.com>
2014-06-18 10:45:15 -0600

    move scripts. to bottom of doc, add example features to body data-features

--- index.php
+++ index.php
@@ -1,31 +1,24 @@
 <!doctype html>
 
-	<head>
-		<title>Site Name</title>
+<head>
+	<title>Site Name</title>
 
-		<!-- meta -->
-		<meta charset="utf-8">
-		<meta name="robots" content="all">
+	<!-- meta -->
+	<meta charset="utf-8">
+	<meta name="robots" content="all">
 
-		<!-- css -->
-		<link rel="stylesheet" type="text/css" href="css/screen.css" media="screen" />
+	<!-- css -->
+	<link rel="stylesheet" type="text/css" href="css/screen.css" media="screen" />
 
-		<!-- iphone -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- iphone -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-		<!--[if lt IE 9]>
-			<link rel="stylesheet" type="text/css" href="css/screen-lt-ie9.css" media="screen" />
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" type="text/css" href="css/screen-lt-ie9.css" media="screen" />
 		<![endif]-->
-		
-		<!-- js  -->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script>window.jQuery || document.write("<script src='js/jquery-1.11.0.min.js'>\x3C/script>")</script>
-		<script src="js/modernizr.custom.75579.js"></script>
-		<script src="js/placeholders.min.js"></script>
-		<script src="js/scripts.js"></script>
 	
-	 	<!-- respond.js -->
-	 	<script src="js/respond.min.js"></script>
+		<!-- respond.js -->
+		<script src="js/respond.min.js"></script>
 		
 		<!-- favicons and touch icons -->
 		<link href="favicons/apple-touch-icon-xlarge.png" rel="apple-touch-icon-precomposed" sizes="144x144" type="image/png" />
@@ -38,21 +31,31 @@
 	</head>
 
 
-	<body data-features="MyFeature">
+	<body data-features="AnimatedScrolling OpenClose TapHover">
 
-			<header class="main-header">
-				<nav>
-					<ul>
-						<li><a href="#">Nav Item</a></li>
-					</ul>
-				</nav>
-			</header>
+		<header class="main-header">
+			<nav>
+				<ul>
+					<li><a href="#">Nav Item</a></li>
+					<li><a href="#">Nav Item</a></li>
+					<li><a href="#">Nav Item</a></li>
+					<li><a href="#">Nav Item</a></li>
+				</ul>
+			</nav>
+		</header>
 		
-			<div id="main" class="main" role="main">
+		<div id="main" class="main" role="main">
 		
 
-			</div> <!-- main -->
-
+		</div> <!-- main -->
+		
+		<!-- js -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script>window.jQuery || document.write("<script src='js/jquery-1.11.0.min.js'>\x3C/script>")</script>
+		<script src="js/modernizr.custom.75579.js"></script>
+		<script src="js/placeholders.min.js"></script>
+		<script src="js/scripts.js"></script>
+	
 	</body>
-</html>
+	</html>
 	
